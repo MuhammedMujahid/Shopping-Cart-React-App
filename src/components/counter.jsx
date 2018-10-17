8c5400499995d5583ae4620bc9d07e6f3c3842cc
Muhammed Mujahid <fakharm@hotmail.com>
2018-10-17 00:54:28 -0400

    binded this

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,21 +2,53 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
   };
+
+  //   constructor(){
+  //       super();
+  //       this.handleIncrement = this.handleIncrement.bind(this); //since "this" keyword is defined here, you can perma bind it to use in a function
 
   //   styles = {
   //     fontSize: 50,
   //     fontWeight: "bold"
   //   };
+  renderTags() {
+    if (this.state.tags.length === 0) {
+      return <p>GG NO RE, ARRAY IS EMPTY</p>;
+    }
+
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li> // dynamically renders list by using map function, added key to uniquely identify elements.
+          // only necessary to uniquely identify within a list
+        ))}
+      </ul>
+    );
+  }
+
+  handleIncrement = () => {
+    console.log("WOW MUCH INCREMENT", this);
+  };
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={this.handleIncrement} //NOTE: passed reference instead of call for onClick
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+
+        {this.renderTags()}
       </div>
     );
   }
+
+  //helper methods
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
     classes += this.state.count === 0 ? "warning" : "primary";
