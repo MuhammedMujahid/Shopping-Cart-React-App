176403c1bc8af78e22f9927c811e335fd193ed90
Muhammed Mujahid <fakharm@hotmail.com>
2018-12-09 15:37:37 -0500

    Updates

---
+++ precache-manifest.abec6bee1f9ad4e375487fbd8ae6be28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e410b663fa2f53a91283",
+    "url": "/Shopping-Cart-React-App/static/css/main.b779c80e.chunk.css"
+  },
+  {
+    "revision": "e410b663fa2f53a91283",
+    "url": "/Shopping-Cart-React-App/static/js/main.e410b663.chunk.js"
+  },
+  {
+    "revision": "a031819272626c08382f",
+    "url": "/Shopping-Cart-React-App/static/css/1.bdad24f7.chunk.css"
+  },
+  {
+    "revision": "a031819272626c08382f",
+    "url": "/Shopping-Cart-React-App/static/js/1.a0318192.chunk.js"
+  },
+  {
+    "revision": "a097cb109316674ed253",
+    "url": "/Shopping-Cart-React-App/static/js/runtime~main.a097cb10.js"
+  },
+  {
+    "revision": "85cc0598393a363e235d006c0180c161",
+    "url": "/Shopping-Cart-React-App/index.html"
+  }
+];
